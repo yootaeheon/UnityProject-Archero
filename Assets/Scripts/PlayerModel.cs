ece09897c6f9f490c9ecf84d1a5be15400e490b1
TaeHeon Ryoo <taroni55@naver.com>
2024-09-27 10:25:38 +0900

    [FEAT] PlayerModel
    MVP 패턴 적용, 모델 추가

---
+++ Assets/Scripts/PlayerModel.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerModel : MonoBehaviour
+{
+    [SerializeField] float hp;
+    public float HP { get { return hp; } set { hp = value; } }
+
+    [SerializeField] float moveSpeed;
+    public float MoveSpeed {  get { return moveSpeed; } set { moveSpeed = value; } }
+
+    [SerializeField] float shootSpeed;
+    public float ShootSpeed { get {return shootSpeed; } set { shootSpeed = value; } }
+
+    [SerializeField] float arrowSpeed;
+    public float ArrowSpeed { get { return arrowSpeed; } set { arrowSpeed = value; } }
+}
